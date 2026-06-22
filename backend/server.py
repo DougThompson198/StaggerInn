e0999fb5d34d0188918fa5c7482b05a3883857dc
D.Thompson <dougthompson198@gmail.com>
2026-06-22 01:43:33 -0400

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -15,9 +15,6 @@ import uuid
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
-mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
 
 SHARED_PASSWORD = os.environ.get('CABIN_SHARED_PASSWORD', 'cottage2026')
 
